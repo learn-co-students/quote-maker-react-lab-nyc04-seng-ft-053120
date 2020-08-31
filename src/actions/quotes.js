6593e59cbe627f59fff875cee449b0baff504e12
AlveeM <alvee.akand@outlook.com>
2020-08-30 22:06:50 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,30 @@
 // TODO: Create action creators as defined in tests
+import ACTIONS from './actions'
+
+export const addQuote = (quote) => {
+  return {
+    type: ACTIONS.ADD_QUOTE,
+    quote,
+  }
+}
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: ACTIONS.REMOVE_QUOTE,
+    quoteId,
+  }
+}
+
+export const upvoteQuote = (quoteId) => {
+  return {
+    type: ACTIONS.UPVOTE_QUOTE,
+    quoteId,
+  }
+}
+
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: ACTIONS.DOWNVOTE_QUOTE,
+    quoteId,
+  }
+}
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
-
   render() {
     return (
       <div>
@@ -15,11 +15,9 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+               {
+                 this.props.quotes.map(quote => <QuoteCard key={quote.id} quote={quote} removeQuote={this.props.removeQuote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} />)
+               }
             </div>
           </div>
         </div>
@@ -28,5 +26,12 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => ({ quotes: state.quotes })
+
+const mapDispatchToProps = {
+  removeQuote,
+  upvoteQuote,
+  downvoteQuote,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
